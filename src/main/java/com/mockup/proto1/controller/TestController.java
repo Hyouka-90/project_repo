9d6dc629bedf7948e5f7020eecfa76fa6ea81d07
Phollakrit K <kanaphaphit1990@gmail.com>
2020-05-10 17:40:41 +0700

    update example jwt

--- src/main/java/com/mockup/proto1/controller/TestController.java
+++ src/main/java/com/mockup/proto1/controller/TestController.java
@@ -8,15 +8,30 @@ import com.mockup.proto1.service.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.mockup.proto1.config.JwtTokenUtil;
+
+import com.mockup.proto1.model.JwtRequest;
+import com.mockup.proto1.model.JwtResponse;
+
+import org.springframework.http.ResponseEntity;
+//mport org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.DisabledException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 
 @RestController
 @RequestMapping("/test")
@@ -24,11 +39,41 @@ public class TestController {
 	
 	
 	@Autowired
-	private ProductService 	productService;
+    private ProductService 	productService;
+    
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    //@Autowired
+    //private AuthenticationManager authenticationManager;
 	
 	@GetMapping("/getAllProduct")
     public List<Products> getAllProduct() {
         return productService.findAll();
+    }
+
+   
+    @PostMapping("/genToken")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+         //authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        //final UserDetails userDetails = userDetailsService .loadUserByUsername(authenticationRequest.getUsername());
+        Map<String, Object> claims = new HashMap<>();
+        final String token = doGenerateToken(claims, "admin");
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @GetMapping("/verifyToken")
+    public String verifyToken(@RequestParam(value = "token") String token,@RequestParam(value = "username") String username  ){
+
+        boolean result = jwtTokenUtil.validateToken(token, username);
+
+        return String.valueOf(result);
+    }
+
+  
+
+    public String doGenerateToken (Map<String, Object> claims, String subject) {
+        return jwtTokenUtil.doGenerateToken(claims, subject);
     }
 	
 	@GetMapping("/product/{id}")
